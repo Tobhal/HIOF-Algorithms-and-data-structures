8b91c9487a78388077449f98d2b1a5f5cb6ba4ce
Tobias Hallingstad <tobias@hallingstad.me>
2022-01-10 23:50:28 +0100

    Done task 5.1 and 5.2

--- src/main/kotlin/oblig5/HashLinear.kt
+++ src/main/kotlin/oblig5/HashLinear.kt
@@ -18,19 +18,74 @@ class HashLinear(
 
     fun hash(s: String): Int = abs(s.hashCode()) % hashLength
 
+
+    /*
+    Oppgave 1. Last come, first served
+     */
+    // Function to move a element at a index position to another position
+    private fun move(i: Int, originalInt: Int) {
+        // Calculate the next index to use
+        val next = (i + 1) % hashLength
+
+        // If the next index is the start index, we have looped, so the array is full
+        if (next == originalInt) {
+            System.err.println("\nHash table is full, canceling")
+            exitProcess(0)
+        }
+
+        // If the next element is empty move the current element to the next element to the new position
+        // Sett the current element to null
+        if (hashTable[next] == null) {
+            hashTable[next] = hashTable[i]
+            hashTable[i] = null
+        } else {    // There is a element on this position so move that.
+            move(next, originalInt)
+        }
+    }
+
     fun insert(s: String) {
         val h = hash(s)
-        var next = h
-        while (hashTable[next] != null) {
+        while (hashTable[h] != null) {
+            // Changes this to move the value if there is something there
+            move(h, h)
             numProbes++
-            next = (next + 1) % hashLength
-            if (next == h) {
+        }
+        hashTable[h] = s
+    }
+
+    /*
+    Oppgave 2. Robin Hood
+        Based on: https://programming.guide/robin-hood-hashing.html
+     */
+    // Return the index difference of the hash
+    private fun hashDiff(s: String, i: Int): Int = abs(hash(s) - i)
+
+    fun insert2(s: String) {
+        var workingString = s
+        var h: Int = hash(s)
+        var next: Int = h
+        while (hashTable[next] != null) {
+            // If string at index is larger or the same, so move on
+            if (hashTable[next]?.let { hashDiff(it, next) }!! >= hashDiff(s, next)) {
+                next = (next + 1) % hashLength
+            } else {    // The string at the index is smaller, swap positions
+                val tmp = hashTable[next]
+                hashTable[next] = workingString
+                // Needs to be a null check to make Kotlin happy
+                if (tmp != null) {
+                    workingString = tmp
+                    h = hash(workingString)
+                }
+                numProbes++
+            }
+
+            // If the inserting has looped the hash list is full return error
+            if (next == hash(s)) {
                 System.err.println("\nHash table is full, canceling")
                 exitProcess(0)
             }
         }
-
-        hashTable[next] = s
+        hashTable[next] = workingString
     }
 
     fun search(s: String): Boolean {
@@ -49,7 +104,6 @@ class HashLinear(
 
         return false
     }
-
 }
 
 fun hashLinerMain(args: Array<String>) {
@@ -69,8 +123,17 @@ fun hashLinerMain(args: Array<String>) {
 
     val hL = HashLinear(hashLength)
 
-    while (input.hasNext()) {
-        hL.insert(input.nextLine())
+    hL.insert2("Kake")
+    hL.insert2("kake")
+
+    var readInput = true
+    while (readInput) {
+        print("Inn: ")
+        val str = readln()
+        if (str == "")
+            readInput = false
+        else
+            hL.insert2(str)
     }
 
     println("Hash length : $hashLength")
